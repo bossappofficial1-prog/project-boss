5cad9f890209c96cb3f418cc2ce28ea41ae23596
Baghaztra <baghaztra21@gmail.com>
2025-06-22 23:43:30 +0700

    Add basic layout, header, footer, and product card components
    Introduces AppHeader, AppFooter, and ProductCard components for the frontend. Updates the main app structure to use layouts and adjusts the color theme. Adds a home page, updates the index page with branding and navigation, and includes a logo image.

---
+++ frontend/app/pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    babi
+  </div>
+</template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-screen">
-    <h1 class="font-bold text-2xl text-(--ui-primary)">
-      Nuxt UI - Starter
-    </h1>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-primary text-white p-4">
+    <!-- Logo -->
+    <NuxtImg
+      src="/images/logo_white.png"
+      width="240"
+      height="240"
+      alt="Logo"
+      class="rounded-full"
+    />
 
-    <div class="flex items-center gap-2">
-      <UButton
-        label="Documentation"
-        icon="i-lucide-square-play"
-        to="https://ui.nuxt.com/getting-started/installation/nuxt"
-        target="_blank"
-      />
+    <!-- Judul -->
+    <h1 class="text-md font-bold mb-6 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium reprehenderit libero unde voluptatibus provident eveniet saepe quisquam repellat, ipsam eius incidunt rerum recusandae voluptate, eum vitae temporibus. Itaque, cumque vitae?</h1>
 
-      <UButton
-        label="GitHub"
-        color="neutral"
-        variant="outline"
-        icon="i-simple-icons-github"
-        to="https://github.com/nuxt/ui"
-        target="_blank"
-      />
-    </div>
+    <UButton
+      to="/home"
+      color="white"
+      variant="solid"
+      size="lg"
+      label="Mulai"
+      class="text-primary font-semibold"
+    />
   </div>
 </template>
+
+<script setup>
+definePageMeta({
+  layout: 'blank'
+})
+</script>
